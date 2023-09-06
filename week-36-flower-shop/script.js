89941a10c059ed41c24973229920fb327d708a0c
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-09-06 13:38:28 +0200

    Added JS Modules

--- week-36-flower-shop/script.js
+++ week-36-flower-shop/script.js
@@ -1,4 +1,4 @@
-const mainContent = document.getElementById("main-content");
+import { createFlower } from "./create-flower.js";
 
 const flower1 = {
   id: 1,
@@ -27,43 +27,5 @@ const flower3 = {
 const allFlowers = [flower1, flower2, flower3];
 
 allFlowers.forEach((x) => {
-  console.log(x.id);
-
-  // Hiver fat i skabelonen fra HTML
-  const template = document.getElementById("template");
-
-  // Clone template
-  const clone = document.importNode(template.content, true);
-
-  // Udfylder img på kopien
-  const img = clone.getElementById("img");
-  img.src = x.imgUrl;
-  img.id = "image-" + x.id;
-
-  // Udfylde minus knappen
-  const minusClone = clone.getElementById("minus-button");
-  minusClone.id = "minus-button-" + x.id;
-
-  // Udfylde nyt ID for amount p-tag
-  const cloneP = clone.getElementById("amount");
-  cloneP.id = "amount-" + x.id;
-
-  // Udfylde nyt ID for amount plus button
-  const addClone = clone.getElementById("add-button");
-  addClone.id = "add-button-" + x.id;
-
-  addClone.addEventListener("click", () => {
-    console.log("hej fra plus knap");
-    cloneP.innerText = Number(cloneP.innerText) + 1;
-  });
-
-  minusClone.addEventListener("click", () => {
-    if (Number(cloneP.innerText)) {
-      cloneP.innerText = Number(cloneP.innerText) - 1;
-      // TODO : Subtract 250kr from total
-    }
-  });
-
-  // Sætter den udfyldte kopi ind på hjemmesiden
-  mainContent.appendChild(clone);
+  createFlower(x);
 });
