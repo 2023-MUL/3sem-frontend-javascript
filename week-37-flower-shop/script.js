5d04b62868d968f665314a5668422a278775ef35
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-09-12 15:36:37 +0200

    preparing for teaching on 13th of sept

---
+++ week-37-flower-shop/script.js
@@ -0,0 +1,18 @@
+import { createFlower } from "./create-flower.js";
+import { fetchFlowers } from "./flowers-api.js";
+
+const form = document.getElementById("create-flower");
+form.addEventListener("submit", (event) => {
+  console.log("Hello from Form");
+  event.preventDefault();
+});
+
+async function load() {
+  const body = await fetchFlowers();
+
+  body.forEach((x) => {
+    createFlower(x);
+  });
+}
+
+load();
